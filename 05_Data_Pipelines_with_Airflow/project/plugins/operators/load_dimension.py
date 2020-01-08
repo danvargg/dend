7d541684449e30f64cf29de6ec0a4f8a9df257a9
Daniel Vargas <devg28@gmail.com>
2020-01-08 10:10:41 -0500

    completes operators.

--- 05_Data_Pipelines_with_Airflow/project/plugins/operators/load_dimension.py
+++ 05_Data_Pipelines_with_Airflow/project/plugins/operators/load_dimension.py
@@ -12,17 +12,29 @@ class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
 
-    @apply_defaults
-    def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
-                 *args, **kwargs):
+    do_sql = """
+        TRUNCATE TABLE {};
+        INSERT INTO {}
+        {};
+        COMMIT;
+    """
 
+    @apply_defaults
+    def __init__(self, redshift_conn_id='', table='', load_sql_stmt='', *args, **kwargs):
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.load_sql_stmt = load_sql_stmt
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        self.log.info(
+            'Loading dimension table {} in Redshift'.format(self.table))
+
+        formatted_sql = LoadDimensionOperator.do_sql.format(
+            self.table,
+            self.table,
+            self.load_sql_stmt)
+
+        redshift.run(formatted_sql)
